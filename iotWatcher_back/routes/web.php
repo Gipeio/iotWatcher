dcff3f9f382e1ec4fd503c56bbd52eaf50832df7
Gil <85408560+Gipeio@users.noreply.github.com>
2024-05-04 19:27:38 +0200

    ajout des routes creer utilisateur et get utilisateurs

--- iotWatcher_back/routes/web.php
+++ iotWatcher_back/routes/web.php
@@ -1,9 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UtilisateurController;
 
 Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
+
+Route::get('/creer-utilisateur', [UtilisateurController::class, 'creerUtilisateur']);
+
+Route::get('/utilisateurs', [UtilisateurController::class, 'listeUtilisateurs']);
 
 require __DIR__.'/auth.php';
